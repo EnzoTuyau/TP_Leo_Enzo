8e5e5635fa7359b4d82888dd6a562201d59dbe0f
2467991 <Ibahimenzo11@gmail.com>
2025-11-12 15:14:40 -0500

    EEE

--- src/main/java/org/example/tp_leo_enzo/MainJavaFX.java
+++ src/main/java/org/example/tp_leo_enzo/MainJavaFX.java
@@ -1,31 +1,72 @@
 package org.example.tp_leo_enzo;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
+import java.util.Random;
 
 public class MainJavaFX extends Application {
-    @Override
-    public void start(Stage stage) throws IOException {
-
-
-        //Bonjour
-
-
-
-
-        Pane pane = new Pane();
-        Scene scene = new Scene(pane, 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+    public static final double WIDTH = 640, HEIGHT = 480;
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        var root = new Pane();
+        var scene = new Scene(root, WIDTH, HEIGHT);
+        var canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        var context = canvas.getGraphicsContext2D();
+
+        Flocon[] flocons = new Flocon[1000];
+
+        for (int i = 0; i < flocons.length; i++) {
+            Random rnd = new Random();
+            int choix = rnd.nextInt(3);
+
+            switch (choix) {
+                case 0 -> flocons[i] = new Flocon();
+                case 1 -> flocons[i] = new Grele();
+                case 2 -> flocons[i] = new FloconOscillant();
+            }
+        }
+
+        var timer = new AnimationTimer() {
+            long lastTime = System.nanoTime();
+
+            @Override
+            public void handle(long now) {
+
+                double deltaTemps = (now - lastTime) * 1e-9;
+
+                // -- Update --
+                for (var flocon : flocons)
+                    flocon.update(deltaTemps);
+
+                // -- Dessin --
+                // Arrière-plan
+                context.setFill(Color.gray(0.2));
+                context.fillRect(0, 0, WIDTH, HEIGHT);
+
+                for (var flocon : flocons)
+                    flocon.draw(context);
+
+                lastTime = now;
+            }
+        };
+        timer.start();
+
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Animations!");
+        primaryStage.show();
     }
 }
