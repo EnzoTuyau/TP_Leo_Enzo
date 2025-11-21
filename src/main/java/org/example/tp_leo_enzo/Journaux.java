ca663e01576f5334a2eec901a8307c7e232e8ddb
2467991 <Ibahimenzo11@gmail.com>
2025-11-21 16:22:41 -0500

    EEE

--- src/main/java/org/example/tp_leo_enzo/Journaux.java
+++ src/main/java/org/example/tp_leo_enzo/Journaux.java
@@ -1,8 +1,21 @@
 package org.example.tp_leo_enzo;
 
+import javafx.geometry.Point2D;
 import javafx.scene.canvas.GraphicsContext;
 
 public class Journaux extends ObjetDuJeu {
+    protected Point2D pos;
+    protected Point2D velocite;
+    protected Point2D acc = new Point2D(0, 1500);
+    protected Point2D taille;
+
+    public Journaux(Point2D pos, Point2D velocite, Point2D taille) {
+        this.pos = pos;
+        this.velocite = velocite;
+        this.taille = taille;
+    }
+
+
 
     @Override
     protected void draw(GraphicsContext context, Camera camera) {
